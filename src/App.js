b4bc4ff554d104f9db8e187bdf9560abd6a78184
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-01-19 17:15:42 +0530

    Dynamic web page using Hooks, useState

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ReactDom from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-
 const App=()=>{
     return(
        <div className="app">
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,19 @@
 import RestaurentCard from "./RestaurentCard"
 import {resList} from "../utils/resList"
+import { useState } from "react";
 const Body=()=>{
+  const [listOfRestaurents,setListOfRestaurents]=useState(resList);
     return(
         <div className="body">
-            <div className="search">
-                Search
-            </div>
+            <button className="filter-btn" onClick={ ()=>{
+              const filteredResList=resList.filter( (res)=>(res.info.rating.aggregate_rating>4))
+              setListOfRestaurents(filteredResList);
+            }}>
+                  Top Rated Restaurents
+                  </button>
             <div className="res-container">
                 {
-                  resList.map( (restaurent)=>(
+                  listOfRestaurents.map( (restaurent)=>(
                     <RestaurentCard key={restaurent.info.resId} resData={restaurent}/>
                   ))
                 }
