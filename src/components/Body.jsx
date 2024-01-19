5617b0393f4cd4c4b3d726ecb73bfecb73c41a88
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-01-19 22:32:14 +0530

    useEffect and shimmer UI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,24 @@
 import RestaurentCard from "./RestaurentCard"
 import {resList} from "../utils/resList"
 import { useState } from "react";
+import Shimmer from "./Shimmer";
 const Body=()=>{
   const [listOfRestaurents,setListOfRestaurents]=useState(resList);
+
+  // useEffect( ()=>{
+  //   fetchData()
+  // },[]);
+
+  const fetchData= async()=>{
+    const data=await fetch("");
+
+    const json=await data.json();
+  };
+
+  if(listOfRestaurents.length===0)
+  {
+    return <Shimmer/>
+  }
     return(
         <div className="body">
             <button className="filter-btn" onClick={ ()=>{
